d3ff71427e694e74cd5439b5a2c432f69e1e21da
Akash Sharma <122847899+aakashsharma003@users.noreply.github.com>
2024-08-12 13:08:06 +0530

    Time: 183 ms (18.72%), Space: 52.2 MB (15.85%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,23 +2,24 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         set<vector<int>>st;
-        unordered_map<long long, int>mp;
         int n = nums.size();
-        for(int i = 0;i < n - 2;i++){
-            for(int j = i + 1;j < n - 1;j++){
-              for(int k = j + 1;k < n;k++){
-                long long val = target;
-                val -= nums[i];
-                val -= nums[j];
-                val -= nums[k];
-                  if(mp.find(val) != mp.end()){
-                    vector<int>temp({nums[i], nums[j], nums[k], target - (nums[i] + nums[j] + nums[k])});
-                    sort(temp.begin(), temp.end());
-                    st.insert(temp);
-                  }
-                  mp[nums[k]]++; 
-              }
-              mp.clear();
+        sort(nums.begin(), nums.end());
+        for(int i = 0;i < n - 3;i++){
+            for(int j = i + 1;j < n - 2;j++){
+               int k = j + 1, l = n - 1;
+               while(k < l){
+                //  if(nums[i] >= target) break;
+                 long long cursum = nums[i];
+                 cursum += nums[k];
+                 cursum += nums[l];
+                 cursum += nums[j];
+                 if(cursum < target) k++;
+                 else if(cursum == target){ st.insert({nums[i], nums[j], nums[k], nums[l]});
+                 k++;l--;
+                //  break;
+                 }
+                 else l--;  
+               }
             }
             
         }
