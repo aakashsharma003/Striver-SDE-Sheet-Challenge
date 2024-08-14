9961c04df23937b33fbd95e0c1f336e85a25bd19
Akash Sharma <122847899+aakashsharma003@users.noreply.github.com>
2024-08-14 20:33:27 +0530

    Time: 12 ms (12.57%), Space: 7.6 MB (72.96%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     int dp[101][101];
-    int solve(int i, int j){
-         if(i == 0 && j == 0) return 1;
-         if(i < 0 || j < 0) return 0;
-         if(dp[i][j] != -1) return dp[i][j];
-        int top = 0, left = 0;
-        if(i >= 1)
-        top += solve(i - 1, j);
-        if(j >= 1)
-        left += solve(i, j - 1);
-        return dp[i][j] = top + left;
-    }
     int uniquePaths(int m, int n) {
-        memset(dp, -1, sizeof(dp));
-      return solve(m - 1, n - 1);  
+        memset(dp, 0, sizeof(dp));
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) continue;
+                int top = 0, left = 0;
+                if (i >= 1)
+                    top += dp[i - 1][j];
+                if (j >= 1)
+                    left += dp[i][j - 1];
+
+                dp[i][j] = top + left;
+            }
+        }
+
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
+                cout<<dp[i][j]<<" ";
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 };
