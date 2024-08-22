6f034e560705eb8d53d65ff60b0494cc89f91517
Akash Sharma <122847899+aakashsharma003@users.noreply.github.com>
2024-08-22 15:51:45 +0530

    Time: 13 ms (72.31%), Space: 48.4 MB (59.99%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -2,20 +2,20 @@ class Solution {
 
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> mp = new HashMap<>();
-        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+        PriorityQueue<Map.Entry<Integer, Integer>> minhp = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
         for(var num: nums){
           mp.put(num, mp.getOrDefault(num, 0) + 1);
         }
         for(Map.Entry<Integer,Integer> entry: mp.entrySet()){
-           pq.add(entry);
-           if(pq.size() > k){
-            pq.remove();
+           minhp.add(entry);
+           if(minhp.size() > k){
+            minhp.remove();
            }
         }
         int ans[] = new int[k];
         while(k > 0){
-            // System.out.println(pq.peek().getValue() +" "+  pq.peek().getKey());
-            ans[k - 1] = pq.poll().getKey();
+            // System.out.println(minhp.peek().getValue() +" "+  minhp.peek().getKey());
+            ans[k - 1] = minhp.poll().getKey();
             k--;
         }
 
