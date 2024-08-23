5e54c86c7560b97d106462a48479d468c41ddfdb
Akash Sharma <122847899+aakashsharma003@users.noreply.github.com>
2024-08-23 22:36:21 +0530

    Time: 16 ms (18.81%), Space: 42.2 MB (33.49%) - LeetHub

---
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.java
@@ -0,0 +1,69 @@
+class Solution {
+    static int gcd(int a, int b)
+    {
+        // if b=0, a is the GCD
+        if (b == 0)
+            return a;
+ 
+        // call the gcd() method recursively by
+        // replacing a with b and b with
+        // difference(a,b) as long as b != 0
+        else
+            return gcd(b, Math.abs(a - b));
+    }
+    public String fractionAddition(String exp) {
+        int den = 1, num = 0;
+         if(exp.charAt(0) != '-'){
+          exp = '+' + exp;
+        }
+        System.out.println(exp);
+        for(int i = 0;i < exp.length() - 1;i++){
+            if(exp.charAt(i) == '/'){
+                int val = 0, j = i + 1;
+                while(j < exp.length() ){
+                    if(exp.charAt(j) == '+' || exp.charAt(j) == '-') break;
+                    System.out.println((int)(exp.charAt(j) - '0'));
+                    val = val*10 + (int)(exp.charAt(j) - '0');
+                    j++;
+                }
+                // System.out.println(val);
+                den *= val;
+            }
+        }
+       System.out.println(den);
+
+        for(int i = 0;i < exp.length() - 3;){
+            char op = exp.charAt(i);
+            System.out.println(i);
+            int numr = 0, j = i + 1;
+            while(j < exp.length() && exp.charAt(j) != '/'){
+                numr = numr*10 +  exp.charAt(j) - '0';
+                j++;
+            }
+            if(exp.charAt(j) == '/')
+            j++;
+            int d = 0;
+            while(j < exp.length()){ 
+                if(exp.charAt(j) == '+' || exp.charAt(j) == '-') break;
+                d = d*10 + (exp.charAt(j) - '0');
+                j++;
+            }
+            // System.out.println("d" + d);
+            int val = (den / d) * numr;
+           
+            num = (op == '+' ? num + val : num - val);
+            //  System.out.println(den + " " + val);
+             i = j;
+        }
+        // System.out.println(num + "/" + den);
+        int div = gcd(num, den);
+        num /= div;
+        den /= div;
+        String ans = "";
+        ans += num;
+        ans += '/';
+        ans = (num == 0 ? ans + 1: ans + den);
+        System.out.println(ans);
+       return ans; 
+    }
+}
