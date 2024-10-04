c3eb498a621b91709b416f5be70d50ac067ae162
Akash Sharma <122847899+aakashsharma003@users.noreply.github.com>
2024-10-04 18:32:30 +0530

    Time: 68 ms (36.8%), Space: 56.5 MB (23.4%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -4,16 +4,13 @@ public:
         sort(skill.begin(), skill.end());
         int n = skill.size();
         int sum = skill[0] + skill[n - 1];
+        long long ans = (long long)(skill[0] * skill[n - 1]);
         for(int i = 1;i < n/2;i++){
            if(skill[i] + skill[n - 1 - i] != sum){
              return -1;
            }
+            ans += (long long)(skill[i] * skill[n - 1 - i]);
         }
-        long long ans = 0;
-         for(int i = 0;i < n/2;i++){
-        //   cout<<skill[i]<< " " <<skill[n - 1 - i]<<endl;
-           ans += (long long)(skill[i] * skill[n - 1 - i]);
-         }
         return ans; 
     }
 };
