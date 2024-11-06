879b1f762df993f26feaab8e6a74a297bd553cdc
Akash Sharma <122847899+aakashsharma003@users.noreply.github.com>
2024-11-06 23:49:34 +0530

    Time: 14 ms (14.39%), Space: 34 MB (12.77%) - LeetHub

---
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canSortArray(vector<int>& nums) {
+       vector<vector<int>>grp;
+       for(int i = 0;i < nums.size(); ){
+           grp.push_back({});
+           int setbits = __builtin_popcount(nums[i]);
+           while(i < nums.size() && setbits == __builtin_popcount(nums[i])){
+               grp.back().push_back(nums[i]);
+               i++;
+           }
+       }
+       int prev_max = -1e9;
+       for(auto it: grp){
+           int cur_min = 1e9, cur_max = -1e9;
+           for(auto val: it){
+               cur_min = min(cur_min, val);
+               cur_max = max(cur_max, val);
+           }
+           if(prev_max <= cur_min){
+               prev_max = cur_max;
+           }
+           else
+            return false;
+       }
+       return true;
+    }
+};
